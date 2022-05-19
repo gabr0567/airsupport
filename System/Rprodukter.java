f52450130b70252bbe524da0889bfe57517ae353
Nilaksan Srigopalan <nilaksansri@gmail.com>
2022-05-19 20:14:48 +0200

    Tillægsprodukt indsat data i scene

--- System/Rprodukter.java
+++ System/Rprodukter.java
@@ -1,11 +1,36 @@
+//Nilaksan
 package application;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Rprodukter {
-	private 
+	private int TillægsproduktID; 
+	private float Pris;
+	private String Navn;
+	private boolean Aktiv;
 	
-	public Rprodukter() {
+	
+	public Rprodukter(int TillægsproduktID, float Pris, String Navn, boolean Aktiv){
+		this.TillægsproduktID = TillægsproduktID;
+		this.Pris = Pris;
+		this.Navn = Navn;
+		this.Aktiv = Aktiv;
 		
 	}
+	public StringProperty getNavn() {
+		StringProperty var = new SimpleStringProperty(Navn);
+		return var;
+	}
+		
+	public StringProperty getPris() {
+		StringProperty var = new SimpleStringProperty(""+Pris);
+		return var;
+	}
+	
 	
 	
 }
