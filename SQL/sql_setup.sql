ebef07dd27ae83823a4c70cceeacbe421d48fc2f
Siva0039 <105000733+Siva0039@users.noreply.github.com>
2022-05-24 19:05:34 +0200

    fjernet statusog tilføjet placering
    status er blevet fjernet og placering er blevet tillføjet inkl dummy datas

--- SQL/sql_setup.sql
+++ SQL/sql_setup.sql
@@ -62,24 +62,26 @@ CONSTRAINT PK_Kunde_KundeID PRIMARY KEY CLUSTERED
 CREATE TABLE [Airsupport].[dbo].[Fly]
 (FlyID int NOT NULL,
 Navn varchar(100) NOT NULL,
-Status Bit NOT NULL,
+Placering char(3) NOT NULL,
 Pladser int NOT NULL,
 CONSTRAINT PK_Fly_FlyID PRIMARY KEY CLUSTERED 
 (FlyID)
 )
 
-INSERT INTO Tillægsprodukter (TillægsproduktID, Pris,Navn, Aktiv)
+INSERT INTO [Airsupport].[dbo].[Tillægsprodukter] (TillægsproduktID, Pris,Navn,Aktiv)
 VALUES (1,40,'Allergi', 1), (2, 55, 'Glutenfri', 0),(3, 90, 'Diabetikeren', 1),(4, 250, 'Luksus', 0),
 (5, 25, 'Sodavand', 1),(6, 125, 'Pinot Noir de France 2020', 1),(7, 500, 'Huber Eiswein 2016', 1),
 (8, 350, 'Udsigt og information', 1),(9, 50, 'Is', 1),(10, 650, 'Fotografi', 1),
 (11, 850, 'Speciel bagage håndtering', 1)
 
-INSERT INTO Fly (FlyID, Navn, Pladser, Status)
-VALUES (1, 'Cessna Citation V', 8, 1), (2, 'Cessna Citation V', 8, 1), (3, 'Cessna Citation V', 8, 1),
-(4, 'Cessna Citation V', 8, 1), (5, 'Cessna Citation V', 8, 1)
+INSERT INTO [Airsupport].[dbo].[Fly] (FlyID, Navn, Pladser, Placering)
+VALUES (1, 'Cessna Citation V', 8, 'BLL'), (2, 'Cessna Citation V', 8, 'BLL'), (3, 'Cessna Citation V', 8, 'BLL'),
+(4, 'Cessna Citation V', 8, 'BLL'), (5, 'Cessna Citation V', 8, 'STN')
 
-INSERT INTO Billet (BilletID, KundeID, Navn, Til, Fly, Dato, Sæde, Gate, afgang)
+INSERT INTO [Airsupport].[dbo].[Billet] (BilletID, KundeID, Navn, Til, Fly, Dato, Sæde, Gate, afgang)
 VALUES (412,124,'John', 'CPH', 3, '2022-5-20', 4, '3B', '18:00:00')
+
+
 
 
 
